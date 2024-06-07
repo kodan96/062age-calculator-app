9aba4bdbe087089e2c4e37f9d8e5f2b43fe1a234
kodan96 <farrkas53@gmail.com>
2024-06-08 00:48:56 +0200

    .

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+        countUp: './node_modules/countup.js/dist/countUp.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    mode: 'production',
+};
